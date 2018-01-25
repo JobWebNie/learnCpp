41a0fbae5a3937c1825c0c8404a47457e7314883
JobWebNie <1979510089@qq.com>
2018-01-25 20:18:55 +0800

    学习number的定义及使用

---
+++ static/number.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // 我们知道数字声明需要使用 int,short,long,float和double
+
+    short  s;
+    int    i;
+    long   l;
+    float  f;
+    double d;
+
+    // 数字赋值
+
+    s = 10;
+    i = 100;
+    l = 10000;
+    f = 444.555;
+    d = 39309.987;
+
+    // 打印输出
+
+    cout << "s :" << s << endl;
+    cout << "i :" << i << endl;
+    cout << "l :" << l << endl;
+    cout << "f :" << f << endl;
+    cout << "d :" << d << endl;
+
+    return 0;
+}
